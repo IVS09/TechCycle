965ed6e173cf07727651dc987fc27dd270925d19
mrlap <inveleiro@gmail.com>
2024-10-20 00:33:15 +0200

    Login Activity lines adjustment wide commit

--- app/src/main/java/com/mrlapidus/techcycle/Onboarding.kt
+++ app/src/main/java/com/mrlapidus/techcycle/Onboarding.kt
@@ -1,20 +1,34 @@
 package com.mrlapidus.techcycle
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.mrlapidus.techcycle.databinding.ActivityOnboardingBinding
 
 class Onboarding : AppCompatActivity() {
+
+    private lateinit var binding: ActivityOnboardingBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_onboarding)
+
+        binding = ActivityOnboardingBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        binding.buttonEmailLogin.setOnClickListener {
+            val intent = Intent(this@Onboarding, Login::class.java)
+            startActivity(intent)
+        }
+
     }
 }

c4035f96539007531d62a9e10653afcf0d9c28ec
mrlap <inveleiro@gmail.com>
2024-10-20 19:58:48 +0200

    recovery layout and links commit

--- app/src/main/java/com/mrlapidus/techcycle/Recuperacion.kt
+++ app/src/main/java/com/mrlapidus/techcycle/Recuperacion.kt
@@ -1,39 +1,20 @@
 package com.mrlapidus.techcycle
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.mrlapidus.techcycle.databinding.ActivityLoginBinding
 
-class Login : AppCompatActivity() {
-
-    private lateinit var binding: ActivityLoginBinding
-
+class Recuperacion : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-
-        binding = ActivityLoginBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
+        setContentView(R.layout.activity_recuperacion)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-
-        binding.registerTextView.setOnClickListener {
-            val intent = Intent(this@Login, Registro::class.java)
-            startActivity(intent)
-        }
-
-        binding.forgotPasswordTextView.setOnClickListener {
-            val intent = Intent(this@Login, Recuperacion::class.java)
-            startActivity(intent)
-        }
-
     }
 }

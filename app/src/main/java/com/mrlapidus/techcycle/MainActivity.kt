3560c1fee1ce92ce00ea7b5d91f574a9fa613419
mrlap <inveleiro@gmail.com>
2024-10-26 23:05:30 +0200

    fragments commit

--- app/src/main/java/com/mrlapidus/techcycle/MainActivity.kt
+++ app/src/main/java/com/mrlapidus/techcycle/MainActivity.kt
@@ -2,7 +2,13 @@ package com.mrlapidus.techcycle
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import com.mrlapidus.techcycle.databinding.ActivityMainBinding
+import com.mrlapidus.techcycle.fragments.AdsFragment
+import com.mrlapidus.techcycle.fragments.ChatFragment
+import com.mrlapidus.techcycle.fragments.HomeFragment
+import com.mrlapidus.techcycle.fragments.ProfileFragment
+import com.mrlapidus.techcycle.fragments.ShopFragment
 
 class MainActivity : AppCompatActivity() {
 
@@ -14,5 +20,27 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
 
+        // Configurar el BottomNavigationView con Fragments
+        binding.bottomNavigationView.setOnItemSelectedListener { menuItem ->
+            val fragment = when (menuItem.itemId) {
+                R.id.nav_home -> HomeFragment()
+                R.id.nav_chat -> ChatFragment()
+                R.id.nav_shop -> ShopFragment()
+                R.id.nav_ads -> AdsFragment()
+                R.id.nav_profile -> ProfileFragment()
+                else -> null
+            }
+            fragment?.let { loadFragment(it) }
+            true
+        }
+        // Cargar el fragmento inicial (opcionalmente HomeFragment)
+        if (savedInstanceState == null) {
+            loadFragment(HomeFragment())
+        }
+    }
+    private fun loadFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragmentContainer, fragment)
+            .commit()
     }
 }

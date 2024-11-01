45214237cbe06975290e7b44f680564a58943908
mrlap <inveleiro@gmail.com>
2024-11-01 18:54:19 +0100

    enable biding commit

--- app/src/main/java/com/mrlapidus/techcycle/Registro.kt
+++ app/src/main/java/com/mrlapidus/techcycle/Registro.kt
@@ -5,12 +5,19 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.mrlapidus.techcycle.databinding.ActivityRegistroBinding
 
 class Registro : AppCompatActivity() {
+
+    private lateinit var binding: ActivityRegistroBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_registro)
+
+        binding = ActivityRegistroBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)

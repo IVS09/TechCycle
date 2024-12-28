b0ccea50573b4ad774bb1859eadd5175d406c203
mrlap <inveleiro@gmail.com>
2024-12-28 20:05:34 +0100

    button logout logic commit

--- app/src/main/java/com/mrlapidus/techcycle/fragments/ProfileFragment.kt
+++ app/src/main/java/com/mrlapidus/techcycle/fragments/ProfileFragment.kt
@@ -1,23 +1,40 @@
 package com.mrlapidus.techcycle.fragments
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.mrlapidus.techcycle.R
-
+import androidx.fragment.app.Fragment
+import com.google.firebase.auth.FirebaseAuth
+import com.mrlapidus.techcycle.Login
+import com.mrlapidus.techcycle.databinding.FragmentProfileBinding
 
 class ProfileFragment : Fragment() {
 
+    private var _binding: FragmentProfileBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false)
+        _binding = FragmentProfileBinding.inflate(inflater, container, false)
+        val view = binding.root
+
+        // Configurar el botón de cerrar sesión
+        binding.logoutButton.setOnClickListener {
+            FirebaseAuth.getInstance().signOut()
+            val intent = Intent(activity, Login::class.java)
+            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+            startActivity(intent)
+        }
+
+        return view
     }
 
-
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }

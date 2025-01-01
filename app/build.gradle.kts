62d8cf64e4e7cdda139b103654b4b052853b33c0
mrlap <inveleiro@gmail.com>
2025-01-01 18:15:41 +0100

    install dependencies glide commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,6 +51,7 @@ dependencies {
     implementation(libs.firebase.auth)
     implementation(libs.firebase.database)
     implementation(libs.google.auth)
+    implementation(libs.glide)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- app/src/main/java/com/mrlapidus/techcycle/fragments/ProfileFragment.kt
+++ app/src/main/java/com/mrlapidus/techcycle/fragments/ProfileFragment.kt
@@ -38,3 +38,4 @@ class ProfileFragment : Fragment() {
         _binding = null
     }
 }
+

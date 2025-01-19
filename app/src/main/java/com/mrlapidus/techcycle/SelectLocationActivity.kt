8d351cd5f29536c5f2daeb3957638cd06d665099
mrlap <inveleiro@gmail.com>
2025-01-20 00:06:07 +0100

    layout activity_select_location.xml  id:main correct commit

--- app/src/main/java/com/mrlapidus/techcycle/SelectLocationActivity.kt
+++ app/src/main/java/com/mrlapidus/techcycle/SelectLocationActivity.kt
@@ -11,7 +11,7 @@ class SelectLocationActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_select_location)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.root)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets

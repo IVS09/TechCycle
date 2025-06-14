ca2c8ca18155fcd3b85c8a4aee968374c603efc0
mrlap <inveleiro@gmail.com>
2025-06-14 05:09:05 +0200

    modif ad optimize imports resolve commit

--- app/src/main/java/com/mrlapidus/techcycle/ads/EditAd.kt
+++ app/src/main/java/com/mrlapidus/techcycle/ads/EditAd.kt
@@ -9,7 +9,6 @@ import android.content.pm.PackageManager
 import android.net.Uri
 import android.os.Bundle
 import android.provider.MediaStore
-import android.util.Log
 import android.view.LayoutInflater
 import android.widget.ArrayAdapter
 import android.widget.TextView
